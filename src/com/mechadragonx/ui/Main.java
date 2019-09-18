3e8eec67b0550cee0944e028083359f726924360
MechaDragonX <sonicraghav@gmail.com>
2019-09-17 22:21:25 -0700

    Add MouseListner to see if mouse is in a window

--- src/com/mechadragonx/ui/Main.java
+++ src/com/mechadragonx/ui/Main.java
@@ -3,6 +3,8 @@ package com.mechadragonx.ui;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Main
 {
--- src/com/mechadragonx/ui/Window.java
+++ src/com/mechadragonx/ui/Window.java
@@ -2,21 +2,26 @@ package com.mechadragonx.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
-public class Window extends JPanel
+public class Window extends JPanel implements MouseListener
 {
     private static Random rng = new Random();
 
     private Dimension size;
     private Point position;
     private Color color;
+    private boolean mouseEntered = false;
+    private boolean mouseDown = false;
 
     public Window(Dimension size)
     {
         this.size = size;
         position = new Point(rng.nextInt(size.width), rng.nextInt(size.height));
         color = new Color(rng.nextFloat(), rng.nextFloat(), rng.nextFloat());
+       this.addMouseListener(this);
     }
 
     public Dimension getSize()
@@ -42,8 +47,58 @@ public class Window extends JPanel
         gfx.setColor(color);
         gfx.fillRect(position.x, position.y, size.width, size.height);
     }
-    public void click()
+
+    public boolean isMouseWithinComponent(Component c)
     {
-        position = MouseInfo.getPointerInfo().getLocation();
+        Point mousePos = MouseInfo.getPointerInfo().getLocation();
+        Rectangle bounds = c.getBounds();
+        bounds.setLocation(c.getLocationOnScreen());
+        return bounds.contains(mousePos);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+        mouseEntered = isMouseWithinComponent((Component) e.getSource());
+        if(mouseEntered)
+        {
+            System.out.println("in");
+        }
+        else
+        {
+            System.out.println("out");
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+        mouseEntered = isMouseWithinComponent((Component) e.getSource());
+        if(mouseEntered)
+        {
+            System.out.println("in");
+        }
+        else
+        {
+            System.out.println("out");
+        }
     }
 }
